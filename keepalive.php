b57fe23078093fdb8d4ff0359487e1d0e11da54a
Eric Hynds <eric@hynds.net>
2010-03-10 18:51:09 -0500

    complete rewrite.  added polling requests

--- keepalive.php
+++ keepalive.php
@@ -1,3 +1,1 @@
-This file is called by jquery.idletimer.js to keep the server-side session alive.
-
-You may want to do something with your session scope here.
+OK
--- src/jquery.idletimeout.js
+++ src/jquery.idletimeout.js
@@ -1,61 +1,149 @@
-$(function(){
+(function($){
 	
-	var $bar = $("#idletimeout"), // id of the warning div
-		$countdown = $bar.find('span'), // span tag that will hold the countdown value
-		redirectAfter = 30, // number of seconds to wait before redirecting the user
-		redirectTo = 'timeout.htm', // URL to relocate the user to once they have timed out
-		keepAliveURL = 'keepalive.php', // URL to call to keep the session alive, if the link in the warning bar is clicked
-		expiredMessage = 'Your session has expired.  You are being logged out for security reasons.', // message to show user when the countdown reaches 0
-		running = false, // var to check if the countdown is running
-		timer; // reference to the setInterval timer so it can be stopped
-	 
-	// start the idle timer.  the user will be considered idle after 2 seconds (2000 ms)
-	$.idleTimer(2000);
-	
-	// bind to idleTimer's idle.idleTimer event
-	$(document).bind("idle.idleTimer", function(){
+	$.idleTimeout = function(options, callback){
 		
-		// if the user is idle and a countdown isn't already running
-		if( $.data(document,'idleTimer') === 'idle' && !running ){
-			var counter = redirectAfter;
-			running = true;
+		options = $.extend({
 		
-			// set inital value in the countdown placeholder
-			$countdown.html( redirectAfter );
+			// number of seconds after user is idle to show the warning
+			warningLength: 30,
 			
-			// show the warning bar
-			$bar.slideDown();
+			// url to call to keep the session alive while the user is active
+			keepAliveURL: "",
 			
-			// create a timer that runs every second
-			timer = setInterval(function(){
-				counter -= 1;
+			// the response from keepAliveURL must equal this text:
+			serverResponseEquals: "OK",
+			
+			// user is considered idle after this many seconds.  10 minutes default
+			idleAfter: 600,
+			
+			// a polling request will be sent to the server every X seconds
+			interval: 60,
+			
+			// number of failed polling requests until we abort this script
+			failedRequests: 5,
+			
+			// callback to fire when the session is resumed (by clicking the resume link)
+			onTimeout: function(){
+				window.location = "timeout.htm";
+			}
+		}, options);
+		
+		
+		var IdleTimeout = {
+			init: function(){
+				var self = this;
 				
-				// if the counter is 0, redirect the user
-				if(counter === 0){
-					$bar.html( expiredMessage );
-					window.location.href = redirectTo;
-				} else {
-					$countdown.html( counter );
-				};
-			}, 1000);
-		};
-	});
+				this.countdownOpen = false;
+				this._startTimer();
+				this.failedRequests = options.failedRequests;
+				
+				// start the idle timer
+				$.idleTimer(options.idleAfter * 1000);
+				
+				// once the user becomes idle
+				$(document).bind("idle.idleTimer", function(){
+					
+					// if the user is idle and a countdown isn't already running
+					if( $.data(document, 'idleTimer') === 'idle' && !self.countdownOpen ){
+					
+						self._stopTimer();
+						self.countdownOpen = true;
+						
+						// call the idle callback
+						self._idle();
+					}
+				});
+			},
+			
+			_idle: function(){
+				var self = this,
+					$bar = $("#idletimeout"),
+					$countdown = $bar.find("span"),
+					timer,
+					counter = options.warningLength;
+				
+				$bar.slideDown();
+				
+				// set inital value in the countdown placeholder
+				$countdown.html( counter );
 	
-	// if the continue link is clicked..
-	$bar.find("a").click(function(){
+				// show the warning bar
+				$bar.slideDown();
+	
+				// create a timer that runs every second
+				timer = setInterval(function(){
+					counter -= 1;
+					
+					if(counter === 0){
+						options.onTimeout();
+						clearInterval(timer);
+					} else {
+						$countdown.html( counter );
+					};
+					
+				}, 1000);
+				
+				// if the continue link is clicked..
+				$bar.find("a").click(function(e){
+					e.preventDefault();
+					
+					clearInterval(timer); // stop the countdown
+					$bar.slideUp(); // hide the warning bar
+					self.countdownOpen = false; // stop countdown
+					self._startTimer(); // start up the timer again
+				});
+			},
+			
+			_startTimer: function(){
+				var self = this;
+				
+				this.timer = setInterval(function(){
+					self._keepAlive();
+				}, options.interval * 1000);
+			},
+				
+			_stopTimer: function(){
+				
+				// reset the failed requests counter
+				this.failedRequests = options.failedRequests;
+				
+				// stop the timer
+				clearInterval(this.timer);
+			},
+			
+			// fires if the keepalive request failed
+			_failed: function(){
+				this.failedRequests--;
+			},
+			
+			_keepAlive: function(){
+				var self = this;
+				
+				// if too many requests failed, abort
+				if(!this.failedRequests){
+					this._stopTimer();
+					return;
+				}
+				
+				$.ajax({
+					timeout: 250,
+					url: options.keepAliveURL,
+					error: function(){
+						self._failed();
+					},
+					success: function(response){
+					
+						// the response from the server must equal OK
+						if($.trim(response) !== options.serverResponseEquals){
+							self._failed();
+						}
+					}
+				});
+			}
+		}
 		
-		// stop the timer
-		clearInterval(timer);
-		
-		// stop countdown
-		running = false;
-		
-		// hide the warning bar
-		$bar.slideUp();
-		
-		// ajax call to keep the server-side session alive
-		$.get( keepAliveURL );
-		
-		return false;
-	});
-});
+		// run this thang
+		IdleTimeout.init();
+ 	}
+	
+})(jQuery);

e2c0fedd4255cc9fbd6d861821d2eecac466082e
Eric Hynds <eric@hynds.net>
2010-03-13 14:25:49 -0500

    resume link binding should be in init(); exposed options in $.idleTimeout.options

--- src/jquery.idletimeout.js
+++ src/jquery.idletimeout.js
@@ -1,55 +1,8 @@
 (function($){
 	
-	$.idleTimeout = function(options){
+	$.idleTimeout = function(element, resume, options){
 		
-		options = $.extend({
-		
-			// the "warning" element, and the link inside to resume
-			warningElement: "#idletimeout",
-			resumeElement: "#idletimeout a",
-		
-			// number of seconds after user is idle to show the warning
-			warningLength: 30,
-			
-			// url to call to keep the session alive while the user is active
-			keepAliveURL: "",
-			
-			// the response from keepAliveURL must equal this text:
-			serverResponseEquals: "OK",
-			
-			// user is considered idle after this many seconds.  10 minutes default
-			idleAfter: 600,
-			
-			// a polling request will be sent to the server every X seconds
-			pollingInterval: 60,
-			
-			// number of failed polling requests until we abort this script
-			failedRequests: 5,
-			
-			// the keepalive AJAX call timeout in MILLISECONDS! 
-			AJAXTimeout: 250,
-			
-			/*
-				Callbacks
-				"this" refers to the #idletimeout element.
-			*/
-			// callback to fire when the session is resumed (by clicking the resume link)
-			onTimeout: function(){},
-			
-			// fires when the user becomes idle
-			onIdle: function(){},
-			
-			// fires during each second of warningLength
-			onCountdown: function(){},
-			
-			// fires when the user resumes the session
-			onResume: function(){},
-			
-			// callback to fire when the script is aborted due to too many failed requests
-			onAbort: function(){}
-			
-		}, options);
-
+		options = $.extend({}, $.idleTimeout.options, options);
 		
 		var IdleTimeout = {
 			init: function(){
@@ -57,8 +10,8 @@
 				
 				// cache elements once the DOM is ready
 				$(function(){
-					self.warning = $(options.warningElement);
-					self.resume = $(options.resumeElement);
+					self.warning = $(element);
+					self.resume = $(resume);
 				});
 				
 				this.countdownOpen = false;
@@ -81,12 +34,21 @@
 						self._idle();
 					}
 				});
+				
+				// bind continue link
+				this.resume.bind("click", function(e){
+					e.preventDefault();
+					
+					window.clearInterval(self.countdown); // stop the countdown
+					self.countdownOpen = false; // stop countdown
+					self._startTimer(); // start up the timer again
+					options.onResume.call( warning ); // call the resume callback
+				});
 			},
 			
 			_idle: function(){
 				var self = this,
 					warning = this.warning[0],
-					timer, 
 					counter = options.warningLength;
 				
 				// fire the onIdle function
@@ -96,27 +58,17 @@
 				options.onCountdown.call(warning, counter);
 				
 				// create a timer that runs every second
-				timer = window.setInterval(function(){
+				this.countdown = window.setInterval(function(){
 					counter -= 1;
 					
 					if(counter === 0){
-						window.clearInterval(timer);
+						window.clearInterval(self.countdown);
 						options.onTimeout.call(warning);
 					} else {
 						options.onCountdown.call(warning, counter);
 					}
 					
 				}, 1000);
-				
-				// if the continue link is clicked..
-				this.resume.bind("click", function(e){
-					e.preventDefault();
-					
-					window.clearInterval(timer); // stop the countdown
-					self.countdownOpen = false; // stop countdown
-					self._startTimer(); // start up the timer again
-					options.onResume.call( warning ); // call the resume callback
-				});
 			},
 			
 			_startTimer: function(){
@@ -128,7 +80,6 @@
 			},
 				
 			_stopTimer: function(){
-				
 				// reset the failed requests counter
 				this.failedRequests = options.failedRequests;
 				
@@ -142,7 +93,7 @@
 				// if too many requests failed, abort
 				if(!this.failedRequests){
 					this._stopTimer();
-					options.onAbort.call( this.warning[0] );
+					options.onAbort.call( this.element );
 					return;
 				}
 				
@@ -166,5 +117,47 @@
 		// run this thang
 		IdleTimeout.init();
 	};
+	
+	var $.idleTimeout.options = {
+		// number of seconds after user is idle to show the warning
+		warningLength: 30,
+		
+		// url to call to keep the session alive while the user is active
+		keepAliveURL: "",
+		
+		// the response from keepAliveURL must equal this text:
+		serverResponseEquals: "OK",
+		
+		// user is considered idle after this many seconds.  10 minutes default
+		idleAfter: 600,
+		
+		// a polling request will be sent to the server every X seconds
+		pollingInterval: 60,
+		
+		// number of failed polling requests until we abort this script
+		failedRequests: 5,
+		
+		// the keepalive AJAX call timeout in MILLISECONDS! 
+		AJAXTimeout: 250,
+		
+		/*
+			Callbacks
+			"this" refers to the #idletimeout element.
+		*/
+		// callback to fire when the session is resumed (by clicking the resume link)
+		onTimeout: function(){},
+		
+		// fires when the user becomes idle
+		onIdle: function(){},
+		
+		// fires during each second of warningLength
+		onCountdown: function(){},
+		
+		// fires when the user resumes the session
+		onResume: function(){},
+		
+		// callback to fire when the script is aborted due to too many failed requests
+		onAbort: function(){}
+	}
 	
 })(jQuery);
